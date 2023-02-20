78295f3544740377626042b02bf66e8435785b31
Mizael Pereira <46569836+mizaelp@users.noreply.github.com>
2023-02-20 18:13:20 +0000

    message

---
+++ script.js
@@ -0,0 +1,27 @@
+const button = document.getElementById("runaway-btn");
+const buttonYes = document.querySelector('.shake')
+
+const animateMove = (element, prop, pixels) =>
+  anime({
+    targets: element,
+    [prop]: `${pixels}px`,
+    easing: "easeOutCirc"
+  });
+
+["mouseover", "click"].forEach(function (el) {
+  button.addEventListener(el, function (event) {
+    const top = getRandomNumber(window.innerHeight - this.offsetHeight);
+    const left = getRandomNumber(window.innerWidth - this.offsetWidth);
+
+    animateMove(this, "left", left).play();
+    animateMove(this, "top", top).play();
+  });
+});
+
+const getRandomNumber = (num) => {
+  return Math.floor(Math.random() * (num + 1));
+};
+
+buttonYes.addEventListener('click', () => {
+  document.querySelector('img').style.display = 'block'
+})
